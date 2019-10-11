1436880c2dbae6aacd655fcf6073346c9c952dae
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-10-11 20:34:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Header from './components/Header';
+import Producto from './components/Producto';
+import ListaProductos from './components/ListaProductos';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+  <Router>
+    <Header/>
+    <main className="container mt-5">
+    <Switch>
+      <Route exact path="/productos" component={ListaProductos}/>
+      <Route exact path="/nuevo_producto" component={Producto}/>
+      <Route exact path="/productos/editar?id=:id" component={Producto}/>
+    </Switch>
+    </main>
+    <p className="mt-4 p2 text-center">Todos los derechos reservados</p>
+  </Router>
   );
 }
 
