9d2958b44c9a126ae0f95a1318e0c444dca00b52
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-10-11 20:34:48 +0200

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <Link to="/productos" className="navbar-brand">React CRUD & Routing</Link>
+
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <NavLink to="/productos" className="nav-link" activeClassName="active">Productos</NavLink>
+                    </li>
+
+                    <li className="nav-item">
+                        <NavLink to="/nuevo_producto" className="nav-link" activeClassName="active">Nuevo Producto</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
