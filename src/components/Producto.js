aace740e77ae31d1e0352cee290ca8db1ce10077
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-10-11 20:34:54 +0200

    Create Producto.js

---
+++ src/components/Producto.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Producto = () => {
+    return (
+        <div>
+            <h1 className="text-center">Agrego/Edito Producto</h1>
+        </div>
+    );
+};
+
+export default Producto;
