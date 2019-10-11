769b6d1aa4cd96e62f6bce67852a5e72d775081f
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-10-11 20:34:51 +0200

    Create ListaProductos.js

---
+++ src/components/ListaProductos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListaProductos = () => {
+    return (
+        <div>
+            <h1 className="text-center">Lista producto</h1>
+        </div>
+    );
+};
+
+export default ListaProductos;
