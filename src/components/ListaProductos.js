31d68235426bb5f1b3f95a8c61d04ab6ae0dbf4d
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-10-13 20:45:56 +0200

    Update ListaProductos.js

--- src/components/ListaProductos.js
+++ src/components/ListaProductos.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import ElementoListaProducto from './ElementoListaProducto';
 
-const ListaProductos = () => {
+const ListaProductos = ({productos}) => {
     return (
-        <div>
+        <Fragment>
             <h1 className="text-center">Lista producto</h1>
-        </div>
+            <ul className="list-group mt-5">
+                {productos.map(producto => (
+                    <ElementoListaProducto 
+                    key={producto.id}
+                    producto={producto}/>
+                ))}
+            </ul>
+        </Fragment>
     );
 };
 
