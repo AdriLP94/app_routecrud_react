dfeeb673fcf4cf3dcafa3891c2fed6aab770da23
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-10-13 20:25:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import axios from 'axios';
 import Header from './components/Header';
 import Producto from './components/Producto';
 import ListaProductos from './components/ListaProductos';
 
 function App() {
+
+  const [productos, guardarProductos] = useState([]);
+
+  useEffect(()=>{
+    const consultaApi = async () => {
+      const resultado = await axios.get('http://localhost:4000/restaurant');
+      guardarProductos(resultado.data);
+    }
+    consultaApi();
+  },[])
+
   return (
   <Router>
     <Header/>
     <main className="container mt-5">
     <Switch>
-      <Route exact path="/productos" component={ListaProductos}/>
+      <Route exact path="/productos" render={ () => 
+        <ListaProductos productos={productos}/>
+      } />
       <Route exact path="/nuevo_producto" component={Producto}/>
-      <Route exact path="/productos/editar?id=:id" component={Producto}/>
+      <Route exact path="/productos/editar/:id" component={Producto}/>
     </Switch>
     </main>
     <p className="mt-4 p2 text-center">Todos los derechos reservados</p>
